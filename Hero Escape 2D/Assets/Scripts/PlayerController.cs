67ad6fa9a48bc5acb11bff9d23726fac22b1b625
carlojean16 <jeancs14@hotmail.com>
2016-04-20 00:37:40 -0300

    First Commit and Upload

---
+++ Hero Escape 2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//PARAMETROS
+	// Declaracao de variaveis/parametros/propriedades
+	// Tipo + Nome ;
+
+	public 	string	nomePersonagem;
+	public 	int 	idadePersonagem;
+	public	float 	alturaPersonagem;
+	public	bool 	personagemAtivo;
+
+	//METODOS
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
